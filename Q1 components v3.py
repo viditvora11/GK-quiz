2d8fa979dfb6c768dcd2a107d4e0810429e19cdd
viditvora11 <78886954+viditvora11@users.noreply.github.com>
2021-04-01 19:45:20 +1300

    Add files via upload

---
+++ Q1 components v3.py
@@ -0,0 +1,19 @@
+#Component 1 asking name, age, yearlvl and v1
+'''
+There will be no question iincluded I will be adding the question only the final
+version because This is only for the components.
+'''
+#Asking Name
+name = input("Please enter your name : ")
+print("Hello",name)
+
+#Asking Age 
+age = input("Please enter your age : ")
+print("Your age is",age)
+
+#Asking Year level
+yearlvl = input("Please enter your year level : ")
+print("Your are in year",yearlvl)
+
+#Asking Status
+status = input("Please enter y to start the quiz and n to stop the quiz.")
