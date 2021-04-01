b9cdb19b5fc045e0b8541e762372b53225ff5d1e
viditvora11 <78886954+viditvora11@users.noreply.github.com>
2021-04-01 20:08:30 +1300

    Add files via upload

---
+++ Components/Quiz v1 components v2.py
@@ -0,0 +1,19 @@
+#Component 1 asking name, age, yearlvl and v1
+'''
+There will be no question iincluded I will be adding the question only the final
+version because This is only for the components.
+'''
+#Asking Name
+name = str(input("Please enter your name : "))
+print("Hello",name)
+
+#Asking Age 
+age = int(input("Please enter your age : "))
+print("Your age is",age)
+
+#Asking Year level
+yearlvl = int(input("Please enter your year level : "))
+print("Your are in year",yearlvl)
+
+#Asking Status
+status = str(input("Please enter y to start the quiz and n to stop the quiz."))
---
+++ Components/Quiz v1 components v4.py
@@ -0,0 +1,32 @@
+#Component 1 asking name, age, yearlvl and v1
+'''
+There will be no question iincluded I will be adding the question only the final
+version because This is only for the components.
+'''
+#Asking Name
+while True:
+    name = input("Please enter your name : ")
+    if name.isalpha():
+        break
+    print("Please enter letter only.")
+
+#Asking Age
+while True:
+    age = input("Please enter your age : ")
+    if age.isnumeric():
+        break
+    print("Please enter numbers only.")
+
+#Asking Year level
+while True:
+    yearlvl = input("Please enter your year level : ")
+    if yearlvl.isnumeric():
+        break
+    print("Please enter numbers only.")
+
+#Asking Status
+while True:
+    status = str(input("Please enter y to start the quiz and n to stop the quiz : "))
+    if status.isalpha():
+        break
+    print("Please enter letter only.")
