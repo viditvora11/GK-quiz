e864ecb7ed548233135ab03fb34163b5d6fa4bd0
viditvora11 <78886954+viditvora11@users.noreply.github.com>
2021-05-31 10:15:55 +1200

    Delete python_assements/Assembled directory

--- python_assements/Assembled/typewriter v2.py
+++
@@ -1,3 +0,0 @@
-import pyautogui
-
-pyautogui.typewrite("'Today is quite cold'",interval = 0.09)
--- python_assements/Assembled/typewriter.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-
-pyautogui.typewrite("'Hello Deep, mukesh andt bhamini this is vidit here and I am here to tell you that I have made this code that auto types everting I want. '",interval = 0.10)
-
-
-
-
