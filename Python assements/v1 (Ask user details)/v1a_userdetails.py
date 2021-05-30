d4298e254cdea236bd7ebb41366de48ba25504ac
viditvora11 <78886954+viditvora11@users.noreply.github.com>
2021-05-31 11:30:59 +1200

    Add files via upload

---
+++ Python assements/v1 (Ask user details)/v1a_userdetails.py
@@ -0,0 +1,18 @@
+#the following code is writen to ask the user detials
+#v1
+#Welcoming users
+print("**************welcome to the genral knowledge quiz*****************")
+print("This quiz is devloped by vidit vora")
+
+#asking name 
+name = input("Please enter your name here : ")
+
+#asking age 
+age = input("Please enter you age here : ")
+
+#asking year level
+yearlvl = input("Please enter your year level here : ")
+
+#printing all users details 
+print("Hello",name,"your age is",age,"and you are in year",yearlvl)
+
---
+++ Python assements/v1 (Ask user details)/v1c_userdetails.py
@@ -0,0 +1,34 @@
+#v1c_user_details
+'''
+this is my next change for asking user details. In the previous program the input
+name doesn't allow anything other than the one it should accept but if any other
+input is used it will say error and not let the user get another chance. I will be
+changing the code to make it allow only the specific inputs and give chances.
+'''
+
+
+#Asking Name using .isalpha to trap errors and used relpace to allow spaces
+while True:
+    global name
+    name = input("Please enter your name here : ")
+    if name.replace(' ','').isalpha():
+        break
+    print("The data type you have used is invalid data type. (Please enter your name in alphabets only.))\n")
+            
+       
+
+#Asking Age using .isnumeric to trap errorsand used relpace to allow spaces
+while True:
+    global age
+    age = input("\nPlease enter your age : ")
+    if age.replace(' ','').isnumeric():
+        if 7< int(age)<125:
+            break
+        else:
+            print('You need to be 7 to 125')
+    else:
+        print("The data type you have used is invalid data type.\n")
+   
+
+
+print("\nHello",name,"are you",age)
