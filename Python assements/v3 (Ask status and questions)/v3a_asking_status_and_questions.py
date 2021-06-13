47ebcd275e242c8a8bff106e0fdc56029b43e781
viditvora11 <78886954+viditvora11@users.noreply.github.com>
2021-06-14 10:42:15 +1200

    Update v3a_asking_status_and_questions.py

--- Python assements/v3 (Ask status and questions)/v3a_asking_status_and_questions.py
+++ Python assements/v3 (Ask status and questions)/v3a_asking_status_and_questions.py
@@ -1,9 +1,9 @@
 #asking status
-status = input("\nAre you ready for testing your general knowledge through our quiz? \n press y to continue or x to exit : ")
-if status == "y" or status == "yes" :
-    print("\nThank you for choosing our quiz for testing your general knowledge lets keep going")
-elif status == 'x' or status == 'no':
-    exit()
+status = input("\nAre you ready for testing your general knowledge through our quiz? \n press y to continue or x to exit : ")#Asking for input
+if status == "y" or status == "yes" : #Using if function
+    print("\nThank you for choosing our quiz for testing your general knowledge lets keep going") #Printing according to the user input
+elif status == 'x' or status == 'no': #Using elif function
+    exit() #Exiting the quiz if user says no
 
 #Right answers to the question
 Q1 = 'shape of you'
@@ -15,16 +15,16 @@ score = 0
 
 #question 1
 print("\nQuestion: 1|score:{}".format(score))
-ans=input("What is the most famous song in the world in 2020?\n\na.shape of you\nb.Despasito\nc.closer\n\nYour answer : ")
+ans=input("What is the most famous song in the world in 2020?\n\na.shape of you\nb.Despasito\nc.closer\n\nYour answer : ") #Asking for input
 if ans == 'shape of you' or ans == 'Shape of you' or ans =='A' or ans == 'a': #Checking answers
-    print("Correct")
-    score+=1
-    print("Your score is",score)
-else:
-    print("Oops incorrect the correct answer is :" ,Q1)
-    if score <=0:#increasing score
+    print("Correct") #Telling if their answer is right
+    score+=1 #Adding score
+    print("Your score is",score) #Showing them the score
+else: #Using else function
+    print("Oops incorrect the correct answer is :" ,Q1) #Showing them the right answer
+    if score <=0:#Keeping the score same as their answer is wrong
         score = 0
-        print("Your score is",score)
+        print("Your score is",score) #Showing them the score
 
 #question 2
 print("\nQuestion: 2|score:{}".format(score))
